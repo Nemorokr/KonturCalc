54d6fab5aa63df48bdf78e84453b9411e6a77738
Nemorokr <46774327+Nemorokr@users.noreply.github.com>
2019-01-17 12:13:09 +0500

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                double x1;
+                double x2;
+                double answ;
+                string action;
+
+                Console.WriteLine("Введите первое число: ");
+                x1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите действие: ");
+                action = Console.ReadLine();
+                Console.WriteLine("Введите второе число: ");
+                x2 = Convert.ToDouble(Console.ReadLine());
+
+                if (action == "+")
+                {
+                    answ = x1 + x2;
+                    Console.WriteLine("Ответ: " + answ);
+                }
+                else if (action == "-")
+                {
+                    answ = x1 - x2;
+                    Console.WriteLine("Ответ: " + answ);
+                }
+                else if (action == "*")
+                {
+                    answ = x1 * x2;
+                    Console.WriteLine("Ответ: " + answ);
+                }
+                else if (action == "/")
+                {
+                    answ = x1 / x2;
+                    Console.WriteLine("Ответ: " + answ);
+                }
+                else
+                    Console.WriteLine("Неверный оператор");
+            }
+        }
+    }
+}
